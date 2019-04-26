5a5136ba0c762bd21f3371889c3edc868a658717
mahdinobar <mahdinobar1993@gmail.com>
2019-04-26 21:36:13 +0200

    model_2 was added

--- codes/general/main_1.py
+++ codes/general/main_1.py
@@ -1,19 +1,16 @@
 """
-GMM K=1for each contour, OpenCV contour, open3d, normalization, no PCA, 3 features (x,y,z)
-it can detect which object is missing at the same frame
-it can <mostly> detect which object among first 7 part frame is assembled with witch one at second 6 part image by seeing
-that the kl_divergance of which detection is increased in comparision with the time there is no assembly
-(<mostly> because e.g except for little cubes with the larges part,...)
+
 """
 
 import cv2 as cv
 import numpy as np
-import math
 from models import model
 from Data import data
 from measure_distance import models_distance
 
-image,depth, cropped_window=data.get_data(None)
+# image,depth, cropped_window=data.get_data(None)
+image=np.load('GMM_10_midpresentation_image_1.npy')
+depth=np.load('GMM_10_midpresentation_depth_1.npy')
 # np.save('GMM_10_midpresentation_image_1.npy', image)
 # np.save('GMM_10_midpresentation_depth_1.npy', depth)
 while 1:
@@ -22,7 +19,9 @@ while 1:
     if key==27:
         break
 
-image_2,depth_2,_=data.get_data(cropped_window)
+# image_2,depth_2,_=data.get_data(cropped_window)
+image_2=np.load('GMM_10_midpresentation_image_2.npy')
+depth_2=np.load('GMM_10_midpresentation_depth_2.npy')
 # np.save('GMM_10_midpresentation_image_2.npy', image_2)
 # np.save('GMM_10_midpresentation_depth_2.npy', depth_2)
 while 1:
@@ -31,8 +30,8 @@ while 1:
     if key==27:
         break
 
-mean_set_1,cov_set_1 = model(image,depth).model_1()
-mean_set_2,cov_set_2=model(image_2,depth_2).model_1()
+mean_set_1,cov_set_1 = model(image,depth).model_2([0,1,2,5])
+mean_set_2,cov_set_2=model(image_2,depth_2).model_2([0,1,2,3])
 
 covariance_distance=models_distance(mean_set_1,cov_set_1,mean_set_2,cov_set_2).covariance_distance()
 mean_distance=models_distance(mean_set_1,cov_set_1,mean_set_2,cov_set_2).mean_distance()
